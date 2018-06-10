1a442a518bb8edea90e113091c5e794a691aabb7
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-06-10 10:35:25 -0300

    DeviceHandler: Fix build
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- DeviceHandler/Android.mk
+++ DeviceHandler/Android.mk
@@ -23,4 +23,20 @@ LOCAL_PACKAGE_NAME := DeviceHandler
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_DEX_PREOPT := false
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v14-preference \
+    android-support-v7-appcompat \
+    android-support-v7-preference \
+    android-support-v7-recyclerview
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    frameworks/support/v14/preference/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/preference/res \
+    frameworks/support/v7/recyclerview/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v14.preference:android.support.v7.appcompat:android.support.v7.preference:android.support.v7.recyclerview
+
 include $(BUILD_PACKAGE)
